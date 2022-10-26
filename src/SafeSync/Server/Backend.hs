28e0b217641947280bc35a39ab54c81854669fb9
Rudy Reynolds <rudy.reyn@gmail.com>
2022-10-25 21:21:09 -0700

    Removed old client app

--- src/SafeSync/Server/Backend.hs
+++
@@ -1,7 +0,0 @@
--- Server/Backend.hs
-module SafeSync.Server.Backend where
-
-import qualified SafeSync.Server.Api as Api
-import qualified SafeSync.Server.MetadataDatabase as MDB
-
-download user_id partition_ids = "{}"
--- src/SafeSync/Server/ObjectStore.hs
+++
@@ -1,9 +0,0 @@
--- 07/27/22
--- ObjectStore.hs
-module SafeSync.Server.ObjectStore where
-
-import Network.Minio
-
-stagePartitions = undefined
-download = undefined
-upload = undefined
